c31db8ca2ba6fe01cbf11785830a9b686f3e707a
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-21 09:49:25 -0500

    models setup

--- models/thought.js
+++ models/thought.js
@@ -1,8 +1,36 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
-const ThoughtSchema = new Schema(
+const thoughtSchema = new Schema(
     {
       thoughtText: {
         type: String,
@@ -19,12 +47,24 @@ const ThoughtSchema = new Schema(
       username:{
         type: String,
         required: true,
-      }
+      },
       reactions: [ReactionSchema]
     },
-
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
   );
   
-  const Thought = model('Thought', ThoughtSchema);
+  thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+
+
+  const Thought = model('Thought', thoughtSchema);
   
   module.exports = Thought;
