65e132521e7ff919e2dd0e8a369165bb3fbafbb8
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-23 10:00:33 -0500

    thoughts controller setup

---
+++ controllers/thought.js
@@ -0,0 +1,63 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    // get all pizzas
+    getAllThought(req, res) {
+      Thought.find({})
+        .populate({
+          path: 'reactions',
+          select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(response => res.json(response))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+  
+    // get one pizza by id
+    getThoughtById({ params }, res) {
+      Pizza.findOne({ _id: params.id })
+        .populate({
+          path: 'reactions',
+          select: '-__v'
+        })
+        .select('-__v')
+        .then(response => res.json(response))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+  
+    // createPizza
+    createThought({ body }, res) {
+      Pizza.create(body)
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
+    },
+  
+    // update pizza by id
+    updateThought({ params, body }, res) {
+      Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(response => {
+          if (!response) {
+            res.status(404).json({ message: 'No Thoughts found with this id!' });
+            return;
+          }
+          res.json(response);
+        })
+        .catch(err => res.json(err));
+    },
+  
+    // delete pizza
+    deleteThought({ params }, res) {
+      Pizza.findOneAndDelete({ _id: params.id })
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
+    }
+  };
+  
+  module.exports = thoughtController;
