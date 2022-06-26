85a54fbd767b71db5918a1adc3b4c7b3b9bbd50c
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-26 16:56:18 -0400

    thought controller setup

--- controllers/thought.js
+++ controllers/thought.js
@@ -58,6 +58,32 @@ const thoughtController = {
         .then(response => res.json(response))
         .catch(err => res.json(err));
     }
-  };
+  }
+   // add reaction
+   addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+}
   
   module.exports = thoughtController;
